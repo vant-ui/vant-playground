8eaecc37ee78c820188e7afb640f010e552b87f1
Nemo <nemoshen94@gmail.com>
2023-12-20 23:00:18 +0800

    fix: editor

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <script setup lang="ts">
-import { Repl, type SFCOptions } from "@vue/repl";
+import {
+  File,
+  Repl,
+  ReplStore,
+  compileFile,
+  type Store,
+  type SFCOptions,
+  type StoreState,
+} from "@vue/repl";
 import Monaco from "@vue/repl/monaco-editor";
 import Header from "./components/Header.vue";
+import { watchEffect } from "vue";
+import { URLSearchParams } from "url";
+import mainCode from "./template/main.vue?raw";
+import welcomeCode from "./template/welcome.vue?raw";
+import { shallowRef, reactive } from "vue";
+const defaultMainFile = "src/App.vue";
+
+const genCdnLink = (pkg: string, version: string | undefined, path: string) => {
+  version = version ? `@${version}` : "";
+  switch ("jsdelivr") {
+    case "jsdelivr":
+      return `https://cdn.jsdelivr.net/npm/${pkg}${version}${path}`;
+    // case 'jsdelivr-fastly':
+    //   return `https://fastly.jsdelivr.net/npm/${pkg}${version}${path}`
+    // case 'unpkg':
+    //   return `https://unpkg.com/${pkg}${version}${path}`
+  }
+};
 
 const sfcOptions: SFCOptions = {
   script: {
@@ -23,14 +49,127 @@ const cdnURL = (path: string) =>
 
 const config = {
   logo: cdnURL("logo.png"),
-  title: 'Vant Playground',
-  links: [{
-    url: 'https://github.com/vant-ui/vant-playground',
-    logo: cdnURL('github.svg'),
-  }],
+  title: "Vant Playground",
+  links: [
+    {
+      url: "https://github.com/vant-ui/vant-playground",
+      logo: cdnURL("github.svg"),
+    },
+  ],
 };
-const versions = []
-const langConfigs = []
+const versions = [];
+const langConfigs = [];
+const MAIN_FILE = "src/PlaygroundMain.vue";
+const APP_FILE = "src/App.vue";
+
+// const query = new URLSearchParams(location.search)
+const _files = {
+  [APP_FILE]: new File(APP_FILE, welcomeCode),
+  [MAIN_FILE]: new File(MAIN_FILE, mainCode, false),
+};
+console.log("_files", _files);
+
+const compiler = shallowRef<typeof import("vue/compiler-sfc")>();
+const state: StoreState = reactive({
+  mainFile: MAIN_FILE,
+  files: _files,
+  activeFile: _files[APP_FILE],
+  errors: [],
+  vueRuntimeURL: "",
+  vueServerRendererURL: "",
+  typescriptVersion: "latest",
+  typescriptLocale: undefined,
+  resetFlip: true,
+});
+const IMPORT_MAP = "import-map.json";
+const genVueLink = (version: string) => {
+  const compilerSfc = genCdnLink(
+    '@vue/compiler-sfc',
+    version,
+    '/dist/compiler-sfc.esm-browser.js'
+  )
+  const runtimeDom = genCdnLink(
+    '@vue/runtime-dom',
+    version,
+    '/dist/runtime-dom.esm-browser.js'
+  )
+  return {
+    compilerSfc,
+    runtimeDom,
+  }
+}
+async function setVueVersion(version: string) {
+  const { compilerSfc, runtimeDom } = genVueLink(version);
+
+  compiler.value = await import(/* @vite-ignore */ compilerSfc);
+  state.vueRuntimeURL = runtimeDom;
+
+  // eslint-disable-next-line no-console
+  console.info(`[@vue/repl] Now using Vue version: ${version}`);
+}
+// TODO: 目前有一个undefined的报错，需要修复，大概率是左侧编辑器的问题
+const store = reactive<Store>({
+  state,
+  compiler: compiler as any,
+  initialShowOutput: false,
+  initialOutputMode: "preview",
+  init: async () => {
+    await setVueVersion('latest')
+    state.errors = []
+    for (const file of Object.values(state.files)) {
+      compileFile(store, file).then((errs) => state.errors.push(...errs))
+    }
+    watchEffect(() =>
+      compileFile(store, state.activeFile).then((errs) => (state.errors = errs))
+    )
+  },
+  setActive: (filename: string) => {},
+  addFile: (filename: string | File) => {},
+  deleteFile: (filename: string) => {},
+  renameFile: (oldFilename: string, newFilename: string) => {},
+  getImportMap: () => {
+    const deps: Record<string, Dependency> = {
+      vue: {
+        pkg: "@vue/runtime-dom",
+        version: "",
+        path: "/dist/runtime-dom.esm-browser.js",
+      },
+      "@vue/shared": {
+        version: "",
+        path: "/dist/shared.esm-bundler.js",
+      },
+      vant: {
+        version: "",
+        path: "/es/index.mjs",
+        //     vant: "https://fastly.jsdelivr.net/npm/vant@4.8.1",
+      },
+      "@vant/use": {
+        version: "",
+        path: "/dist/index.esm.mjs",
+        //     "@vant/use": "https://fastly.jsdelivr.net/npm/@vant/use/dist/index.esm.mjs",
+      },
+      "@vant/popperjs": {
+        version: "",
+        path: "/dist/index.esm.mjs",
+        // "https://fastly.jsdelivr.net/npm/@vue/shared/dist/index.esm.mjs",
+      },
+    };
+
+    const res = {
+      imports: Object.fromEntries(
+        Object.entries(deps).map(([key, dep]) => [
+          key,
+          genCdnLink(dep.pkg ?? key, dep.version, dep.path),
+        ])
+      ),
+    };
+    console.log("nemo res", res);
+    return res;
+  },
+  customElement: false,
+});
+// watchEffect(() => history.replaceState({}, "", store.serialize()));
+// store.userOptions.styleSource = `https://preview-${store.pr}-element-plus.surge.sh/bundle/index.css`
 </script>
 
 <template>
@@ -38,6 +177,7 @@ const langConfigs = []
     <Header :config="config" :lang-configs="langConfigs" lang="zh-CN" />
     <Repl
       ref="replRef"
+      :store="store"
       :editor="Monaco"
       show-compile-output
       auto-resize
---
+++ src/template/main.vue
@@ -0,0 +1,18 @@
+<script setup>
+// import App from './App.vue'
+import Vant from 'vant'
+import { getCurrentInstance } from 'vue'
+
+let installed = false
+function setupVant() {
+  if (installed) return
+  const instance = getCurrentInstance()
+  instance.appContext.app.use(Vant)
+  installed = true
+}
+setupVant()
+</script>
+
+<template>
+  Hello World
+</template>
